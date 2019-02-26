11c257f73eaae6a4b65a868ea7b7d58201cd4294
shwetastha <shweta.stha1@gmail.com>
2019-02-25 20:15:20 -0500

    Components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { MenuComponent } from './menu/menu.component';
 
 import { MatListModule } from '@angular/material/list';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +26,10 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule 
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
